c955697d003ed3ccac4ec63090016279e2540fb7
Julian Ng <julesnuggy@gmail.com>
2023-10-26 20:00:10 +0100

    Add back in src code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { reverse } from './helpers'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('Nicolas')
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="app">
+      <h1 className="title">Welcome, {name}!</h1>
+      <p className="name-length">
+        Did you know that {name} is {name.length} characters long?!
       </p>
-    </>
+      <p className="name-reversed">
+        Also, {name} backwards is {reverse(name)}
+      </p>
+    </div>
   )
 }
 
